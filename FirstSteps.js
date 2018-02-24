7b2d8e4f94f7fc63dda06cfec0f7ee0c6dd3b0db
ismohT <32553573+ismohT@users.noreply.github.com>
2018-02-24 22:59:49 +0100

    Add files via upload

---
+++ FirstSteps.js
@@ -0,0 +1,21 @@
+/**
+ * FirstSteps.js is the very basic program to send an command to the my home gateway with the help of myhome.js module
+ */
+/**
+ * 
+ */
+var myhome = require('myhome');
+
+// connect with the myhome gateway
+var mhengine = new myhome.engine({
+	host:'192.168.1.35',
+	pass:'12345'
+});
+
+
+//send light office out
+mhengine.sendCommand({
+command:'*1*0*11##'	
+});
+
+
